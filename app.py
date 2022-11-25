21d504dc26c8279960ae55d8b6a1cf0f38fbafe9
iamaakashpal <aakashpal1198@gmail.com>
2022-11-25 13:07:09 +0530

    Exception File Updated

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import sys
 from insurance.logger import logging
+from insurance.exception import InsuranceException
 
 app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
-    logging.info('Logging module testing.')
-    return "Flask Testing"
+    try:
+        raise Exception("Testing Custom Exception")
+    except Exception as e:
+        insurance = InsuranceException(e,sys)
+        logging.info(insurance.error_message)
+        logging.info("We are testing logging and Custom Exception")
+    return "CI CD Pipeline"
 
 if __name__ == '__main__':
     app.run(debug=True)
