cb4c9abb3a8405d0f015a8797c8420bd3ba4810b
iamaakashpal <aakashpal1198@gmail.com>
2022-11-25 11:40:53 +0530

    Logger File Updated

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
+from insurance.logger import logging
 
 app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
+    logging.info('Logging module testing.')
     return "Flask Testing"
 
 if __name__ == '__main__':
