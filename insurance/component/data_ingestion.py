c86ae2ee909adca89152f573fa2c3531491c1f33
iamaakashpal <aakashpal1198@gmail.com>
2022-11-25 22:11:01 +0530

    file added

---
+++ insurance/component/data_ingestion.py
@@ -0,0 +1,19 @@
+from insurance.entity.config_entity import DataIngestionConfig
+from insurance.exception import InsuranceException
+import os,sys
+from insurance.logger import logging
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            logging.info(f"{'='*20}Data Ingestion log started.{'='*20}")
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise InsuranceException(e,sys) 
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            pass
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
--- insurance/config/configuration.py
+++ insurance/config/configuration.py
@@ -1,13 +1,49 @@
 from insurance.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,\
 ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
-
+import os,sys
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+from insurance.util.util import read_yaml_file
+from insurance.constant import *
 
 class Configuartion:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,config_file_path:str = CONFIG_FILE_PATH,current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
+        try:
+            self.config_info = read_yaml_file(file_path = config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            data_ingestion_artifact_dir = os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR,self.time_stamp)
+            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+            
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            
+            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
+
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
+            
+            ingested_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY])
+
+            ingested_train_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY])
+            
+            ingested_test_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY])
+            
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url = dataset_download_url,
+                tgz_download_dir = tgz_download_dir,
+                raw_data_dir = raw_data_dir,
+                ingested_train_dir = ingested_train_dir,
+                ingested_test_dir = ingested_test_dir
+            )
+            logging.info(f"Data Ingestion config:{data_ingestion_config}")
+            return data_ingestion_config
+        except Exception as e:
+            raise InsuranceException(e,sys) from e        
 
     def get_data_validation_config(self) -> DataValidationConfig:
         pass
@@ -25,4 +61,16 @@ class Configuartion:
         pass
 
     def get_training_pipeline_config(self) -> TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR,
+            training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir = artifact_dir)
+            logging.info(f"Training Pipeline config:{training_pipeline_config}")
+            return training_pipeline_config
+
+
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
