f32c58f1cf686105e6a4a2227c0f74548d453d29
iamaakashpal <aakashpal1198@gmail.com>
2022-12-06 21:43:38 +0530

    File Updated

--- insurance/exception/__init__.py
+++ insurance/exception/__init__.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-
 class InsuranceException(Exception):
-
+    
     def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = InsuranceException.get_detailed_error_message(error_message=error_message,error_detail=error_detail)
+        self.error_message=InsuranceException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
 
 
     @staticmethod
@@ -15,14 +16,21 @@ class InsuranceException(Exception):
         error_message: Exception object
         error_detail: object of sys module
         """
-        _,_,exec_tb = error_detail.exc_info()
-        line_number = exec_tb.tb_frame.f_lineno
+        _,_ ,exec_tb = error_detail.exc_info()
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occurred in script :[{file_name}] at line number :[{line_number}] error message :[{error_message}]"
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+        """
         return error_message
 
     def __str__(self):
         return self.error_message
+
 
     def __repr__(self) -> str:
         return InsuranceException.__name__.str()
