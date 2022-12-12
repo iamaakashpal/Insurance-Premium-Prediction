c337cbf1988251c459b252813a39ad028d88d4d9
iamaakashpal <aakashpal1198@gmail.com>
2022-12-12 15:44:25 +0530

    File Updated'

--- insurance/component/data_transformation.py
+++ insurance/component/data_transformation.py
@@ -1,6 +1,6 @@
-# from cgi import test
+from cgi import test
 from sklearn import preprocessing
-from insurance.exception import insuranceException
+from insurance.exception import InsuranceException
 from insurance.logger import logging
 from insurance.entity.config_entity import DataTransformationConfig 
 from insurance.entity.artifact_entity import DataIngestionArtifact,\
@@ -15,3 +15,17 @@ from sklearn.impute import SimpleImputer
 import pandas as pd
 from insurance.constant import *
 from insurance.util.util import read_yaml_file,save_object,save_numpy_array_data,load_data
+
+class DataTransformation:
+
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
+                data_validation_artifact:DataValidationArtifact,
+                data_transformation_config:DataTransformationConfig):
+        try:
+            logging.info(f"# {'='*10} # Data Transformation Log Started. # {'='*10} #")
+            self.data_transformation_config=data_transformation_config
+            self.data_validation_artifact=data_validation_artifact
+            self.data_ingestion_artifact=data_ingestion_artifact
+
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
