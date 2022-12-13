3cd14207fcafda36cb5a68a47d8c4d1714e81ccc
iamaakashpal <aakashpal1198@gmail.com>
2022-12-13 12:57:41 +0530

    File Updated

--- insurance/component/data_transformation.py
+++ insurance/component/data_transformation.py
@@ -1,4 +1,3 @@
-from cgi import test
 from sklearn import preprocessing
 from insurance.exception import InsuranceException
 from insurance.logger import logging
@@ -16,16 +15,111 @@ import pandas as pd
 from insurance.constant import *
 from insurance.util.util import read_yaml_file,save_object,save_numpy_array_data,load_data
 
+
 class DataTransformation:
 
     def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
                 data_validation_artifact:DataValidationArtifact,
                 data_transformation_config:DataTransformationConfig):
         try:
-            logging.info(f"# {'='*10} # Data Transformation Log Started. # {'='*10} #")
+            logging.info(f"# {'='*10} # Data Transformation Log Started . # {'='*10} #")
             self.data_transformation_config=data_transformation_config
             self.data_validation_artifact=data_validation_artifact
             self.data_ingestion_artifact=data_ingestion_artifact
 
         except Exception as e:
             raise InsuranceException(e,sys) from e
+
+    def get_data_transformer_object(self,)-> ColumnTransformer:
+        try:
+            schema_file_path=self.data_validation_artifact.schema_file_path
+
+            dataset_schema=read_yaml_file(file_path=schema_file_path)
+
+            numerical_columns=dataset_schema[NUMERICAL_COLUMN_KEY]
+            categorical_columns=dataset_schema[CATEGORICAL_COLUMN_KEY]
+
+            num_pipeline=Pipeline(steps=[('imputer',SimpleImputer(strategy='most_frequent')),
+                                        ('standard',StandardScaler())])
+            cat_pipeline=Pipeline(steps=[('imputer',SimpleImputer(strategy='most_frequent')),
+                                        ('onehotencoding',OneHotEncoder())])
+
+            preprocessing_obj=ColumnTransformer(transformers=[('numerical_pipeline',num_pipeline,numerical_columns),
+                                                ('categorical_pipeline',cat_pipeline,categorical_columns)]
+                                                )
+            
+            return preprocessing_obj
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
+
+    def initiate_data_transformation(self,)->DataTransformationArtifact:
+        try:
+            logging.info(f"Obtaining preprocessing object.")
+
+            preprocessing_obj=self.get_data_transformer_object()
+            
+            logging.info(f"Obtaining training and test file path.")
+            train_file_path=self.data_ingestion_artifact.train_file_path
+            test_file_path=self.data_ingestion_artifact.test_file_path
+
+            schema_file_path = self.data_validation_artifact.schema_file_path
+            
+            logging.info(f"Loading training and test data as pandas dataframe.")
+            train_df = pd.read_csv(train_file_path)
+            
+            test_df = pd.read_csv(test_file_path)
+
+            schema = read_yaml_file(file_path=schema_file_path)
+
+            target_column_name = schema[TARGET_COLUMN_KEY]
+
+            logging.info(f"Splitting input and target feature from training and testing dataframe.")
+            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
+            input_feature_train_arr= preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+
+            train_arr = np.c_[ input_feature_train_arr, np.array(target_feature_train_df)]
+
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            
+            transformed_train_dir = self.data_transformation_config.transformed_train_dir
+            transformed_test_dir = self.data_transformation_config.transformed_test_dir
+
+            train_file_name = os.path.basename(train_file_path).replace(".csv",".npz")
+            test_file_name = os.path.basename(test_file_path).replace(".csv",".npz")
+
+            transformed_train_file_path = os.path.join(transformed_train_dir, train_file_name)
+            transformed_test_file_path = os.path.join(transformed_test_dir, test_file_name)
+            
+
+            logging.info(f"Saving transformed training and testing array.")
+            
+            save_numpy_array_data(file_path=transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(file_path=transformed_test_file_path,array=test_arr)
+
+            preprocessing_obj_file_path = self.data_transformation_config.preprocessed_object_file_path
+
+            logging.info(f"Saving preprocessing object.")
+            save_object(file_path=preprocessing_obj_file_path,obj=preprocessing_obj)
+
+            data_transformation_artifact = DataTransformationArtifact(
+                is_transformed=True,
+                message="Data transformation successfull.",
+                transformed_train_file_path=transformed_train_file_path,
+                transformed_test_file_path=transformed_test_file_path,
+                preprocessed_object_file_path=preprocessing_obj_file_path
+                )
+            logging.info(f"Data transformationa artifact: {data_transformation_artifact}")
+            return data_transformation_artifact
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
+
+    def __del__(self):
+            logging.info(f"# {'='*10} # Data Transformation log Completed. # {'='*10} # \n\n")
--- insurance/util/util.py
+++ insurance/util/util.py
@@ -95,16 +95,16 @@ def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
 
         dataframe = pd.read_csv(file_path)
 
-        error_messgae = ""
+        error_message = ""
 
 
         for column in dataframe.columns:
             if column in list(schema.keys()):
                 dataframe[column].astype(schema[column])
             else:
-                error_messgae = f"{error_messgae} \nColumn: [{column}] is not in the schema."
-        if len(error_messgae) > 0:
-            raise Exception(error_messgae)
+                error_message = f"{error_message} \nColumn: [{column}] is not in the schema."
+        if len(error_message) > 0:
+            raise Exception(error_message)
         return dataframe
 
     except Exception as e:
