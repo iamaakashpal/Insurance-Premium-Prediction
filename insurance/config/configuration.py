2d591fee5cbf442f93b012d41e07d74b9119ff6a
iamaakashpal <aakashpal1198@gmail.com>
2022-12-17 21:40:18 +0530

    File Updated'

--- insurance/config/configuration.py
+++ insurance/config/configuration.py
@@ -1,51 +1,70 @@
-from insurance.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,\
+from insurance.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig,   \
 ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
-import os,sys
-from insurance.logger import logging
-from insurance.exception import InsuranceException
 from insurance.util.util import read_yaml_file
+from insurance.logger import logging
+import sys,os
 from insurance.constant import *
+from insurance.exception import InsuranceException
+
 
 class Configuartion:
-    def __init__(self,config_file_path:str = CONFIG_FILE_PATH,current_time_stamp:str = CURRENT_TIME_STAMP) -> None:
+
+    def __init__(self,
+        config_file_path:str =CONFIG_FILE_PATH,
+        current_time_stamp:str = CURRENT_TIME_STAMP
+        ) -> None:
         try:
-            self.config_info = read_yaml_file(file_path = config_file_path)
+
+            self.config_info  = read_yaml_file(file_path=config_file_path)
             self.training_pipeline_config = self.get_training_pipeline_config()
             self.time_stamp = current_time_stamp
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
-    def get_data_ingestion_config(self) -> DataIngestionConfig:
+    def get_data_ingestion_config(self) ->DataIngestionConfig:
         try:
             artifact_dir = self.training_pipeline_config.artifact_dir
-            
-            data_ingestion_artifact_dir = os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR,self.time_stamp)
-            
-            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
-            
-            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
-            
-            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
-
-            raw_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
-            
-            ingested_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY])
-
-            ingested_train_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY])
-            
-            ingested_test_dir = os.path.join(ingested_data_dir,data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY])
-            
-            data_ingestion_config = DataIngestionConfig(
-                dataset_download_url = dataset_download_url,
-                tgz_download_dir = tgz_download_dir,
-                raw_data_dir = raw_data_dir,
-                ingested_train_dir = ingested_train_dir,
-                ingested_test_dir = ingested_test_dir
+            data_ingestion_artifact_dir=os.path.join(
+                artifact_dir,
+                DATA_INGESTION_ARTIFACT_DIR,
+                self.time_stamp
             )
-            logging.info(f"Data Ingestion config:{data_ingestion_config}")
+            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            tgz_download_dir = os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
+            )
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY]
+            )
+
+            ingested_data_dir = os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+            )
+            ingested_train_dir = os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+            )
+            ingested_test_dir =os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+            )
+
+
+            data_ingestion_config=DataIngestionConfig(
+                dataset_download_url=dataset_download_url, 
+                tgz_download_dir=tgz_download_dir, 
+                raw_data_dir=raw_data_dir, 
+                ingested_train_dir=ingested_train_dir, 
+                ingested_test_dir=ingested_test_dir
+            )
+            logging.info(f"Data Ingestion config: {data_ingestion_config}")
             return data_ingestion_config
         except Exception as e:
-            raise InsuranceException(e,sys) from e        
+            raise InsuranceException(e,sys) from e
 
     def get_data_validation_config(self) -> DataValidationConfig:
         try:
@@ -93,12 +112,16 @@ class Configuartion:
             )
 
             data_transformation_config_info=self.config_info[DATA_TRANSFORMATION_CONFIG_KEY]
-            
+
+          
+
+
             preprocessed_object_file_path = os.path.join(
                 data_transformation_artifact_dir,
                 data_transformation_config_info[DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY],
                 data_transformation_config_info[DATA_TRANSFORMATION_PREPROCESSED_FILE_NAME_KEY]
             )
+
 
             transformed_train_dir=os.path.join(
             data_transformation_artifact_dir,
@@ -106,11 +129,14 @@ class Configuartion:
             data_transformation_config_info[DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY]
             )
 
+
             transformed_test_dir = os.path.join(
             data_transformation_artifact_dir,
             data_transformation_config_info[DATA_TRANSFORMATION_DIR_NAME_KEY],
             data_transformation_config_info[DATA_TRANSFORMATION_TEST_DIR_NAME_KEY]
+
             )
+
 
             data_transformation_config=DataTransformationConfig(
                 preprocessed_object_file_path=preprocessed_object_file_path,
@@ -123,35 +149,79 @@ class Configuartion:
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
+
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         try:
-            pass
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            model_trainer_artifact_dir=os.path.join(
+                artifact_dir,
+                MODEL_TRAINER_ARTIFACT_DIR,
+                self.time_stamp
+            )
+            model_trainer_config_info = self.config_info[MODEL_TRAINER_CONFIG_KEY]
+            trained_model_file_path = os.path.join(model_trainer_artifact_dir,
+            model_trainer_config_info[MODEL_TRAINER_TRAINED_MODEL_DIR_KEY],
+            model_trainer_config_info[MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY]
+            )
+
+            model_config_file_path = os.path.join(model_trainer_config_info[MODEL_TRAINER_MODEL_CONFIG_DIR_KEY],
+            model_trainer_config_info[MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY]
+            )
+
+            base_accuracy = model_trainer_config_info[MODEL_TRAINER_BASE_ACCURACY_KEY]
+
+            model_trainer_config = ModelTrainerConfig(
+                trained_model_file_path=trained_model_file_path,
+                base_accuracy=base_accuracy,
+                model_config_file_path=model_config_file_path
+            )
+            logging.info(f"Model trainer config: {model_trainer_config}")
+            return model_trainer_config
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
-    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+    def get_model_evaluation_config(self) ->ModelEvaluationConfig:
         try:
-            pass
+            model_evaluation_config = self.config_info[MODEL_EVALUATION_CONFIG_KEY]
+            artifact_dir = os.path.join(self.training_pipeline_config.artifact_dir,
+                                        MODEL_EVALUATION_ARTIFACT_DIR, )
+
+            model_evaluation_file_path = os.path.join(artifact_dir,
+                                                    model_evaluation_config[MODEL_EVALUATION_FILE_NAME_KEY])
+            response = ModelEvaluationConfig(model_evaluation_file_path=model_evaluation_file_path,
+                                            time_stamp=self.time_stamp)
+
+
+            logging.info(f"Model Evaluation Config: {response}.")
+            return response
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
     def get_model_pusher_config(self) -> ModelPusherConfig:
         try:
-            pass
+            time_stamp = f"{datetime.now().strftime('%Y%m%d%H%M%S')}"
+            model_pusher_config_info = self.config_info[MODEL_PUSHER_CONFIG_KEY]
+            export_dir_path = os.path.join(ROOT_DIR, model_pusher_config_info[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY],
+                                           time_stamp)
+
+            model_pusher_config = ModelPusherConfig(export_dir_path=export_dir_path)
+            logging.info(f"Model pusher config {model_pusher_config}")
+            return model_pusher_config
+
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
-    def get_training_pipeline_config(self) -> TrainingPipelineConfig:
+    def get_training_pipeline_config(self) ->TrainingPipelineConfig:
         try:
             training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
             artifact_dir = os.path.join(ROOT_DIR,
             training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
-            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
+            )
 
-            training_pipeline_config = TrainingPipelineConfig(artifact_dir = artifact_dir)
-            logging.info(f"Training Pipeline config:{training_pipeline_config}")
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"Training pipleine config: {training_pipeline_config}")
             return training_pipeline_config
-
-
         except Exception as e:
-            raise InsuranceException(e,sys) from e
+            raise InsuranceException(e,sys) from e 
